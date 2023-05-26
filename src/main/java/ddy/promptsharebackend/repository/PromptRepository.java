9d2977ea7da895801e1bb7fd448a8065ce26422c
Toysin <toysin1203@hotmail.com>
2023-05-27 01:14:02 +0900

    #4-PromptRepository를 빈으로 등록함
    @Repository 아노테이션 추가함

--- src/main/java/ddy/promptsharebackend/repository/PromptRepository.java
+++ src/main/java/ddy/promptsharebackend/repository/PromptRepository.java
@@ -2,6 +2,8 @@ package ddy.promptsharebackend.repository;
 
 import ddy.promptsharebackend.domain.Prompt;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PromptRepository extends JpaRepository<Prompt, Long> {
 }
